f70ce5eafa13d754fa339ac7fb028790bbb2a7aa
RotemLibrati <rotemlibrati1@gmail.com>
2022-07-09 01:46:48 +0300

    Redux

---
+++ src/actions/types.js
@@ -0,0 +1,9 @@
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const REGISTER_SECCUSS = 'REGISTER_SECCUSS';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SECCESS = 'LOGIN_SECCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
---
+++ src/reducers/auth.js
@@ -0,0 +1,44 @@
+import { REGISTER_FAIL,LOGOUT, REGISTER_SECCUSS, USER_LOADED, AUTH_ERROR, LOGIN_SECCESS, LOGIN_FAIL } from "../actions/types";
+
+const initialState ={
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null
+}
+
+export default function(state=initialState, action) {
+    const {type, payload } = action;
+    switch(type) {
+        case USER_LOADED:
+            console.log(state);
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
+        case REGISTER_SECCUSS:
+        case LOGIN_SECCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            };
+        case REGISTER_FAIL:
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
+        default:
+            return state;
+    }
+}
