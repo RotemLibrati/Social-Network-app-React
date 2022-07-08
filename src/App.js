94fdd99518af2803889bed3dd427d941e9c00774
RotemLibrati <rotemlibrati1@gmail.com>
2022-07-09 01:47:33 +0300

    Create app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+
+        <React.Fragment>
+
+          <Navbar />
+
+          <Routes>
+
+
+            <Route exact path='/' element={<Landing />} />
+
+            {/* <section className='container'> */}
+
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+
+            {/* </section> */}
+
+          </Routes>
+          <Alert />
+        </React.Fragment>
+      </Router >
+    </Provider>
   );
 }
 
