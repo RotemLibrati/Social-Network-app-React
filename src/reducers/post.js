9985a8ac3a75e0d690942719536454c673859f0e
RotemLibrati <rotemlibrati1@gmail.com>
2022-07-18 22:56:25 +0300

    Fix delete comment

--- src/reducers/post.js
+++ src/reducers/post.js
@@ -57,7 +57,7 @@ export default function (state = initialState, action) {
                 ...state,
                 post: {
                     ...state.post,
-                    comments: state.post.comments.filter(comment => comment.id !== payload)
+                    comments: state.post.comments.filter(comment => comment._id !== payload)
                 }
             }
         default:
